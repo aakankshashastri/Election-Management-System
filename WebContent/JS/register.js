3046b3c2bfa6921c21a3b53a7976efac1a4f35a3
AakankshaShastri <aakankshashastri1998@gmail.com>
2018-06-05 15:50:59 +0530

    Add files via upload

---
+++ WebContent/JS/register.js
@@ -0,0 +1,34 @@
+function validate()
+{ 
+ var voterid = document.form.voterid.value;
+ var email = document.form.email.value;
+ var username = document.form.username.value; 
+ var password = document.form.password.value;
+ var conpassword= document.form.conpassword.value;
+ 
+ if (voterid==null || voterid=="" || voterid.length < 10)
+ { 
+ alert("Voter ID is not valid"); 
+ return false; 
+ }
+ else if (email==null || email=="")
+ { 
+ alert("Email can't be blank"); 
+ return false; 
+ }
+ else if (username==null || username=="")
+ { 
+ alert("Username can't be blank"); 
+ return false; 
+ }
+ else if(password.length<6)
+ { 
+ alert("Password must be at least 6 characters long."); 
+ return false; 
+ } 
+ else if (password!=conpassword)
+ { 
+ alert("Confirm Password should match with the Password"); 
+ return false; 
+ } 
+ } 
